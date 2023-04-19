8babc03bc13c3260540346ca0f46186939eb9199
Josh Hancock <livingicon@gmail.com>
2023-04-18 20:25:20 -0400

    Add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 // App.tsx
 
 import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className="App">
-    </div>
+    <Router basename="shopping-cart">
+      <div className="App">
+        <Nav 
+        />
+        <Main 
+        />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
