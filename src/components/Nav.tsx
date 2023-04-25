8b3be25377987366f3097df3dad210193ffa9f4f
Josh Hancock <livingicon@gmail.com>
2023-04-24 20:17:03 -0400

    Add styled-components and style nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,23 +2,61 @@
 
 import React from "react";
 import { Link } from "react-router-dom";
-// import './style.css';
+import styled from "styled-components";
 
-const Nav = () => {
+const Nav: React.FC = () => {
 
   return (
-    <nav>
-      <h1 id="appName">Where's Hieronymus?</h1>
-      <ul className="nav-links">
-        <Link className="nav-link" to='/'>
-          <li>Gameboards</li>
-        </Link>
-        <Link className="nav-link" to='/leaderboard'>
-          <li>Leaderboard</li>
-        </Link>
-      </ul>
-    </nav>
+    <NavWrapper>
+      <nav>
+        <h1 id="appName">Where's Hieronymus?</h1>
+        <ul className="nav-links">
+          <Link className="nav-link" to='/'>
+            <li>Gameboards</li>
+          </Link>
+          <Link className="nav-link" to='/leaderboard'>
+            <li>Leaderboard</li>
+          </Link>
+        </ul>
+      </nav>
+    </NavWrapper>
   )
 }
 
+const NavWrapper = styled.nav`
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    background-color: #f0f0f0;
+  }
+
+  h2 {
+    font-size: 1.2rem;
+    margin: 0;
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+    margin: 0 10px;
+  }
+
+  a {
+    color: #333;
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+`
+
 export default Nav;
+
