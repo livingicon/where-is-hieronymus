bd7e528543150ae566b61f0ca61efb0ae16aac16
Josh Hancock <livingicon@gmail.com>
2023-05-03 21:39:37 -0400

    Add LeaderboardFormModal.tsx and filler content

--- src/components/StartGameModal.tsx
+++ src/components/StartGameModal.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+// StartGameModal.tsx
+
+import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
--- src/pages/Gameboards/GameboardOne.tsx
+++ src/pages/Gameboards/GameboardOne.tsx
@@ -1,7 +1,7 @@
 // GameboardOne.tsx
 
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import styled from 'styled-components';
 import Temptation from '../../images/temptation_of_saint_anthony.jpg';
 import Alice from "../../images/TemptAlice.png";
@@ -10,6 +10,8 @@ import Frank from "../../images/TemptFrank.png";
 import checkmark from "../../images/checkmark.png";
 import Timer from "../../components/Timer";
 import TargetBox from "../../components/TargetBox";
+import StartGameModal from "../../components/StartGameModal";
+import LeaderboardFormModal from "../../components/LeaderboardFormModal";
 
 const GameboardOne: React.FC = () => {
   const [isVisible, setIsVisible] = useState<boolean>(true);
@@ -18,7 +20,7 @@ const GameboardOne: React.FC = () => {
   const [dropdownLocation, setDropdownLocation] = useState<{ x: number; y: number } | null>(null);
   const [isDropdownVisible, setIsDropdownVisible] = useState<boolean>(false);
   const [temptCharacters, setTemptCharacters] = useState(["Alice", "TinBird", "Frank"]);
-  
+
   const aliceBox = {left: 434, right: 455, top: 210, bottom: 255};
   const tinBirdBox = {left: 714, right: 730, top: 283, bottom: 305};
   const frankBox = {left: 795, right: 806, top: 340, bottom: 355};
@@ -74,20 +76,10 @@ const GameboardOne: React.FC = () => {
   return (
     <GameboardOneContainer>
       {isVisible &&
-        <ModalWrapper>
-          <section className="modal-main">
-            <h2>Instructions</h2>
-            <p>Your task is to find the following three characters in this painting. 
-              When you click one of the characters in the painting a dropdown will open 
-              showing the available characters and you will click on the one you have found.
-              When you are ready, you may click the start button and the timer will begin.
-              The timer will end when you have found all three characters.</p>
-            <button onClick={() => {startTimer();}}>Start</button>
-            <Link to="/">
-              <button>Cancel</button>
-            </Link>
-          </section>
-        </ModalWrapper>
+        <StartGameModal
+          setIsVisible={setIsVisible}
+          startTimer={startTimer}
+        />
       }
       <InfoWrapper>
         {isTimerStarted && (
@@ -129,6 +121,7 @@ const GameboardOne: React.FC = () => {
           />
         )}
       </ImageWrapper>
+      <LeaderboardFormModal />
     </GameboardOneContainer>
   );
 };
@@ -139,41 +132,6 @@ const GameboardOneContainer = styled.div`
   justify-content: center;
   align-items: center;
   margin-top: 15px;
-`;
-
-const ModalWrapper = styled.div`
-  z-index: 2;
-  display: flex;  
-  justify-content: center;
-  align-items: center;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.85);
-
-  section {
-    width: 75%;
-    border-radius: 5px;
-    background-color: #f0f0f0;
-    font-weight: bold;
-    color: black; // change
-    padding: 10px;
-    display: flex; // change to block?
-    flex-direction: column;
-    text-align: center;
-    overflow: auto;
-    max-height: 100vh;
-  }
-
-  h2 {
-    margin: 0;
-  }
-
-  button {
-    width: 100px;
-  }
 `;
 
 // CHANGE TO ABSOLUTE POSITIONED AND SIZED FOR ZOOM ALWAYS ON SCREEN?
