913e396a46b4b079d26c4ed27d8ef345d7bd256b
Josh Hancock <livingicon@gmail.com>
2023-05-11 18:41:54 -0400

    Add Key component and make GameKey and Timer it's children and update gameboards to use it

---
+++ src/components/Key.tsx
@@ -0,0 +1,74 @@
+// Key.tsx
+
+import React from "react";
+import styled from "styled-components";
+import Timer from "./key/Timer";
+import GameKey from "./key/GameKey";
+
+interface KeyProps {
+  counter: number;
+  setCounter: React.Dispatch<React.SetStateAction<number>>;
+  remainingCharacters: string[];
+  characters: any[][];
+  toggleKeyVisibility: () => void;
+  isKeyVisible: boolean;
+  isTimerStarted: boolean;
+}
+
+const Key: React.FC<KeyProps> = ({
+  counter,
+  setCounter,
+  remainingCharacters,
+  characters,
+  toggleKeyVisibility,
+  isKeyVisible,
+  isTimerStarted,
+}) => { 
+  
+  return (
+    <HideKeyWrapper>
+      <KeyVisibilityButton onClick={toggleKeyVisibility}>
+        {isKeyVisible ? "Hide Key" : "Show Key"}
+      </KeyVisibilityButton>
+      <InfoWrapper isVisible={isKeyVisible}>
+        {isTimerStarted && (
+          <Timer counter={counter} setCounter={setCounter} />
+        )}
+        <GameKey
+          remainingCharacters={remainingCharacters}
+          characters={characters}
+        />
+      </InfoWrapper>
+    </HideKeyWrapper>
+  );
+};
+
+const HideKeyWrapper = styled.div`
+  position: fixed;
+  z-index: 1;
+  top: 1%;
+  left: 1%;
+`;
+
+const KeyVisibilityButton = styled.button`
+  position: relative;
+  font-weight: bold;
+  width: 75px;
+  padding: 2px;
+  background-color: #ca8a04;
+  border: 3px solid black;
+  border-radius: 5px;
+`;
+
+const InfoWrapper = styled.div<{ isVisible: boolean }>`
+  position: relative;
+  width: 160px;
+  height: 75px;
+  background-color: black;
+  color: white;
+  border-radius: 5px;
+  padding: 5px;
+  visibility: ${({ isVisible }) => (isVisible ? 'visible' : 'hidden')};
+`;
+
+export default Key;
--- src/components/key/GameKey.tsx
+++ src/components/key/GameKey.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import styled from "styled-components";
-import checkmark from "../images/checkmark.png";
+import checkmark from "../../images/checkmark.png";
 
 interface GameKeyProps {
   remainingCharacters: string[];
